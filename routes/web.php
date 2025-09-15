ab0a254dd63b9750a7ddff3a1f39fee5f712630c
ThomLahaieClg <202139608@edu.clg.qc.ca>
2025-09-15 19:25:08 -0400

    waaaaaaaaa

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,4 @@ Route::get('/', function () {
     return view('index');
 });
 Route::get('user/signIn', [UserController::class, 'signIn' ]);
-<<<<<<< HEAD
-
-Route::get('/check-username', function ($username) {
-    $exists = User::where('username', $username)->exists();
-    return response()->json(['exists' => $exists]);
-});
-=======
 Route::get('user/profile', [ProfileController::class, 'profile' ]);
->>>>>>> 6145566e971cf31847a80e92e1b99899745cc53d
