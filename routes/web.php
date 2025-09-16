345402204e1ef2ae0d5362a7f60650ec8054573c
mousieur <gabmasse0504@gmail.com>
2025-09-15 20:34:08 -0400

    oupsi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -9,12 +9,9 @@ Route::get('/', function () {
     return view('index');
 });
 Route::get('user/signIn', [UserController::class, 'signIn' ]);
-<<<<<<< HEAD
 
 Route::get('/check-username', function ($username) {
     $exists = User::where('username', $username)->exists();
     return response()->json(['exists' => $exists]);
 });
-=======
 Route::get('user/profile', [ProfileController::class, 'profile' ]);
->>>>>>> 6145566e971cf31847a80e92e1b99899745cc53d
