f7a3b5bd26e5e0ffda3c6dff7347805f6e8946bf
Justin Lessard <202070203@edu.clg.qc.ca>
2025-10-14 17:40:10 -0400

    emil clutch le goat

--- public/js/plinko/sketch.js
+++ public/js/plinko/sketch.js
@@ -1,45 +1,98 @@
 var Engine = Matter.Engine,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Events = Matter.Events; // 🟩 pour écouter les collisions
 
 var engine;
 var world;
 var particles = [];
 var plinkos = [];
 var bounds = [];
+var cubes = []; // 🟩 liste des cubes du bas
 var cols = 19;
-var rows = 19;
+var rows = 16;
 
 function setup() {
-    let canvas = createCanvas(600, 800); //grace a p5 width = 600 et height = 800. Pas besoin de les calls
+    let canvas = createCanvas(600, 625);
     canvas.parent('canvas-container');
     engine = Engine.create();
     world = engine.world;
-    world.gravity.y = 2;
-    newParticle();
-    var spacing = width/cols;
+    world.gravity.y = 1;
+
+    var spacing = width / cols;
+    var centerX = width / 2;
+    var minPlinkos = 3;
+
+    // --- triangle inversé ---
     for (var j = 0; j < rows; j++) {
-        for (var i = 0; i < cols + 1; i++) {
-            var x = i * spacing;
-            if ( j % 2 == 0) {
-                x += spacing/2;
-            }
-            var y = spacing + j * spacing + 50;
+        var numPlinkos = minPlinkos + j;
+        if (numPlinkos > cols) numPlinkos = cols;
+
+        var y = spacing + j * spacing + 30;
+        var totalWidth = (numPlinkos - 1) * spacing;
+
+        for (var i = 0; i < numPlinkos; i++) {
+            var x = centerX - totalWidth / 2 + i * spacing;
             var p = new Plinko(x, y, 6);
             plinkos.push(p);
         }
     }
 
-    var b = new Boundary(width/2, height + 50, width, 100); //le sol
-    bounds.push(b);
+    // --- cubes du bas ---
+    var numCubes = 17;
+    var cubeSize = 25;
+    var margin = 30;
+    var spacing = (width - margin * 2) / numCubes;
+    var startX = margin + spacing / 2;
+    var y = height - cubeSize / 2 - 50;
 
-    for ( var i = 0; i < cols + 2; i++) { //les murs verticaux
-        var x = i * spacing;
-        var h = 70;
-        var w = 10;
-        var y = height - h/2;
-        var b = new Boundary(x * 1.5, y, w, h);
-        bounds.push(b);
+    // 🟩 multiplicateurs symétriques
+    var multipliers = [
+        "500x", "80x", "25x", "10x", "4x", "1x", "0.5x",
+        "0.2x", "0.2x", "0.2x", // centre (3x 0.2x total)
+        "0.5x", "1x", "4x", "10x", "25x", "80x", "500x"
+    ];
+
+    for (var i = 0; i < numCubes; i++) {
+        var x = startX + i * spacing;
+        var cube = new Boundary(x, y, cubeSize, cubeSize);
+        cube.body.label = "cube";
+        cube.multiplier = multipliers[i];
+        bounds.push(cube);
+        cubes.push(cube);
+    }
+
+    // 🟩 Détection de collisions
+    Events.on(engine, "collisionStart", function(event) {
+        var pairs = event.pairs;
+        for (var i = 0; i < pairs.length; i++) {
+            var bodyA = pairs[i].bodyA;
+            var bodyB = pairs[i].bodyB;
+
+            // on vérifie si c’est une particule et un cube
+            if (isParticleAndCube(bodyA, bodyB)) {
+                removeParticle(bodyA.label === "particle" ? bodyA : bodyB);
+            }
+        }
+    });
+}
+
+// 🟩 fonction utilitaire pour détecter particule-cube
+function isParticleAndCube(a, b) {
+    return (
+        (a.label === "particle" && b.label === "cube") ||
+        (a.label === "cube" && b.label === "particle")
+    );
+}
+
+// 🟩 supprime la particule du monde et du tableau
+function removeParticle(body) {
+    for (var i = 0; i < particles.length; i++) {
+        if (particles[i].body === body) {
+            World.remove(world, body);
+            particles.splice(i, 1);
+            break;
+        }
     }
 }
 
@@ -49,11 +102,16 @@ function newParticle() {
 }
 
 function draw() {
-        if (frameCount % 60 == 0) {
-        newParticle();
-    }
     background(51);
     Engine.update(engine);
+
+    // --- Drop automatique des billes si le round est en cours ---
+    if (particlesDropped < totalParticles && frameCount % dropInterval === 0) {
+        newParticle();
+        particlesDropped++;
+    }
+
+    // --- Affichage des particules ---
     for (var i = 0; i < particles.length; i++) {
         particles[i].show();
         if (particles[i].isOffScreen()) {
@@ -62,11 +120,19 @@ function draw() {
             i--;
         }
     }
+
+    // --- Affichage des plinkos ---
     for (var j = 0; j < plinkos.length; j++) {
         plinkos[j].show();
     }
 
+    // --- Affichage des cubes ---
     for (var k = 0; k < bounds.length; k++) {
         bounds[k].show();
+        fill(0);
+        textAlign(CENTER, CENTER);
+        textSize(k === 0 || k === bounds.length - 1 ? 11 : 13);
+        let pos = bounds[k].body.position;
+        text(bounds[k].multiplier, pos.x, pos.y);
     }
 }
