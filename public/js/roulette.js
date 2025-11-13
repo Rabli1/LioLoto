c64add84a8d711c60fc7a9e033c941bb6ad4e49e
ThomLahaieClg <202139608@edu.clg.qc.ca>
2025-11-13 15:50:28 -0500

    fix roulette wallah finito

--- public/js/roulette.js
+++ public/js/roulette.js
@@ -1,3 +1,4 @@
+
 (function () {
     let rouletteNumbers = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
     let redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
@@ -446,6 +447,7 @@
 
             try {
                 settleRouletteBets(winningNumber);
+                clearMat.disabled = false;
             } catch (e) {
                 console.error('Erreur de règlement des mises roulette:', e);
             }
@@ -618,11 +620,17 @@
 
     buttonSpin.addEventListener('click', () => {
         resultSpin = Math.floor(Math.random() * 37);
+        clearMat.disabled = true;
         spinRoulette(resultSpin);
     })
 
     clickRoulette.forEach(betClick => {
         betClick.addEventListener('click', e => {
+            if(buttonSpin && buttonSpin.disabled){
+                e.preventDefault();
+                return;
+            }
+
             const existingToken = betClick.querySelector('.rouletteToken');
 
             if (!window.Balance.miser(tokenValue, { persist: false })) {
